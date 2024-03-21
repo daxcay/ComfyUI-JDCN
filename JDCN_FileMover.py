bc324c517392a7fca9ccc37c6f9314c162a9cb07
jerrydavos <davos.jerry@gmail.com>
2024-03-21 20:19:45 +0530

    Collab Test
    Test

--- JDCN_FileMover.py
+++ JDCN_FileMover.py
@@ -3,6 +3,7 @@ import os
 import shutil
 
 
+
 def create_folder_if_not_exists(folder_path):
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
--- JDCN_VHSFileMover.py
+++ JDCN_VHSFileMover.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 
+
+
 def create_folder_if_not_exists(folder_path):
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
