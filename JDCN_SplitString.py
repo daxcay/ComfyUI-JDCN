531e437fefdb23865b53019fd30c54d2f70080b0
akr1999 <daxtoncaylor@gmail.com>
2024-03-21 18:26:55 +0530

    Update JDCN_SplitString.py
    Updated main function to receive tuple instead of string. also I got to know that we can use any variable type on input

--- JDCN_SplitString.py
+++ JDCN_SplitString.py
@@ -1,4 +1,4 @@
-def split_string(main_string, search_string, from_rear=False, occurrence=1):
+def split_string(main_string, search_string, from_rear=False, occurrence=1, incs=False):
     if from_rear:
         index = main_string.rfind(search_string)
     else:
@@ -14,7 +14,9 @@ def split_string(main_string, search_string, from_rear=False, occurrence=1):
             return main_string, "", -1
     front = main_string[:index]
     rear = main_string[index + len(search_string):]
-    front += search_string
+    if incs:
+        front += search_string
+    
     return front, rear, index
 
 
@@ -32,6 +34,7 @@ class JDCN_SplitString:
                 "SearchFor": ("STRING", {}),
                 "StartFrom": (['front', 'rear'],),
                 "Occurence": ("INT", {"default": 1, "min": 1, "max": 9999}),
+                "IncludeSearch": ("BOOLEAN", {"default": False}),
             },
         }
 
@@ -39,19 +42,18 @@ class JDCN_SplitString:
     RETURN_NAMES = ("Suffix", "Prefix", "FoundAt")
     FUNCTION = "dosplitit"
 
-    def dosplitit(self, MainString, SearchFor, StartFrom, Occurence):
+    def dosplitit(self, MainString, SearchFor, StartFrom, Occurence, IncludeSearch):
 
         if len(MainString) == 0:
             return ("", "", 0)
         if len(SearchFor) == 0:
-            return ("", "", 0)
+            return ("", "", 0) 
 
         from_rear = False
         if StartFrom == "rear":
             from_rear = True
 
-        suffix, prefix, foundat = split_string(
-            MainString, SearchFor, from_rear, Occurence)
+        suffix, prefix, foundat = split_string(MainString, SearchFor, from_rear, Occurence, IncludeSearch)
 
         return (suffix, prefix, foundat)
 
