da75ba06a223f45edb1b303ed17828fecc54736b
akr1999 <daxtoncaylor@gmail.com>
2024-03-23 18:13:02 +0530

    [Node Request] Image Saver.
    Jerry requested this 😎

---
+++ JDCN_ImageSaver.py
@@ -0,0 +1,90 @@
+import os
+import json
+import json
+import numpy as np
+from PIL import Image
+from PIL.PngImagePlugin import PngInfo
+from comfy.cli_args import args
+
+
+def count_files_in_folder(folder_path):
+    file_count = 0
+    for _, _, files in os.walk(folder_path):
+        file_count += len(files)
+    return file_count
+
+
+class JDCN_ImageSaver:
+
+    def __init__(self):
+        self.compression = 4
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "Images": ("IMAGE",),
+                "Directory": ("STRING", {}),
+                "FilenamePrefix": ("STRING", {"default": "Image_"}),
+                "OpenOutputDirectory": ("BOOLEAN", {"default": False}),
+            },
+            "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO"},
+        }
+
+    RETURN_TYPES = ()
+    FUNCTION = "BatchSave"
+    OUTPUT_NODE = True
+
+    def BatchSave(self, Images, Directory, FilenamePrefix, OpenOutputDirectory, prompt=None, extra_pnginfo=None):
+
+        try:
+
+            Directory = Directory
+            FilenamePrefix = FilenamePrefix
+
+            if not os.path.exists(Directory):
+                os.makedirs(Directory)
+
+            lastIndex = count_files_in_folder(Directory)
+            index = 1
+
+            for image in Images:
+                image = image.cpu().numpy()
+                image = (image * 255).astype(np.uint8)
+                img = Image.fromarray(image)
+                metadata = None
+                if not args.disable_metadata:
+                    metadata = PngInfo()
+                    if prompt is not None:
+                        metadata.add_text("prompt", json.dumps(prompt))
+                    if extra_pnginfo is not None:
+                        for x in extra_pnginfo:
+                            metadata.add_text(x, json.dumps(extra_pnginfo[x]))
+                
+                padding = str(lastIndex+index).zfill(4)
+                file_name = f"{FilenamePrefix}_{padding}.png"
+                file_path = os.path.join(Directory, file_name)
+                img.save(file_path, pnginfo=metadata, compress_level=self.compression)
+                index = index + 1
+
+            if(OpenOutputDirectory):
+                try:
+                    os.system(f'explorer "{Directory}"')  
+                    os.system(f'open "{Directory}"')    
+                    os.system(f'xdg-open "{Directory}"')
+                except Exception as e:
+                    print(f"Error opening directory: {e}")
+
+        except Exception as e:
+            print(f"Error saving image: {e}")
+
+        return ()
+
+
+N_CLASS_MAPPINGS = {
+    "JDCN_ImageSaver": JDCN_ImageSaver,
+}
+
+N_DISPLAY_NAME_MAPPINGS = {
+    "JDCN_ImageSaver": "JDCN_ImageSaver",
+}
--- __init__.py
+++ __init__.py
@@ -30,6 +30,7 @@ from .JDCN_VHSFileMover import N_CLASS_MAPPINGS as VHSFileMoverMappins, N_DISPLA
 from .JDCN_ReBatch import N_CLASS_MAPPINGS as ReBatchMappins, N_DISPLAY_NAME_MAPPINGS as ReBatchNameMappings
 from .JDCN_FileMover import N_CLASS_MAPPINGS as FileMoverMappins, N_DISPLAY_NAME_MAPPINGS as FileMoverNameMappings
 from .JDCN_SplitString import N_CLASS_MAPPINGS as SplitStringMappins, N_DISPLAY_NAME_MAPPINGS as SplitStringNameMappings
+from .JDCN_ImageSaver import N_CLASS_MAPPINGS as ImageSaverMappins, N_DISPLAY_NAME_MAPPINGS as ImageSaverNameMappings
 
 NODE_CLASS_MAPPINGS = {}
 NODE_CLASS_MAPPINGS.update(AnyListMappins)
@@ -44,6 +45,7 @@ NODE_CLASS_MAPPINGS.update(VHSFileMoverMappins)
 NODE_CLASS_MAPPINGS.update(ReBatchMappins)
 NODE_CLASS_MAPPINGS.update(FileMoverMappins)
 NODE_CLASS_MAPPINGS.update(SplitStringMappins)
+NODE_CLASS_MAPPINGS.update(ImageSaverMappins)
 
 NODE_DISPLAY_NAME_MAPPINGS = {}
 NODE_DISPLAY_NAME_MAPPINGS.update(AnyListNameNMappings)
@@ -58,5 +60,6 @@ NODE_DISPLAY_NAME_MAPPINGS.update(VHSFileMoverNameMappings)
 NODE_DISPLAY_NAME_MAPPINGS.update(ReBatchNameMappings)
 NODE_DISPLAY_NAME_MAPPINGS.update(FileMoverNameMappings)
 NODE_DISPLAY_NAME_MAPPINGS.update(SplitStringNameMappings)
+NODE_DISPLAY_NAME_MAPPINGS.update(ImageSaverNameMappings)
 
 WEB_DIRECTORY = "./web"
